f6b71618182123d1911eddd6b21f3a37c0567c14
ss-marvin <marvinsawasa@gmail.com>
2025-12-23 23:48:44 +0100

    Export all React components

---
+++ frontend/src/components/index.ts
@@ -0,0 +1,4 @@
+export { FileUpload } from './FileUpload';
+export { DataProfilePanel } from './DataProfilePanel';
+export { QueryChat } from './QueryChat';
+export { ChartRenderer } from './ChartRenderer';
